4d2da787fe66842ba4c521311fbccb5ce59f3d47
Kristen Scheel <kristen.scheel@gmail.com>
2021-07-26 09:32:22 -0400

    added aimScroll

---
+++ wakefast.js
@@ -0,0 +1,27 @@
+const mainTag = document.querySelector("main")
+
+const bodyTag = document.querySelector("body")
+
+mainTag.style.position = "fixed"
+mainTag.style.top = "0px"
+mainTag.style.left = "0px"
+mainTag.style.width = "100%"
+
+let currentScroll = 0
+let aimScroll = 0
+
+const changeScroll = function () {
+  bodyTag.style.height = mainTag.offsetHeight + "px"
+
+  currentScroll = currentScroll + (aimScroll - currentScroll) * 0.05
+
+  mainTag.style.top = (-1 * currentScroll) + "px"
+
+  requestAnimationFrame(changeScroll)
+}
+
+window.addEventListener("scroll", function () {
+  aimScroll = window.pageYOffset
+})
+
+changeScroll()
